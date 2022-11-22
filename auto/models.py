32e3bd68ea160bd10d688f61a20807819e07d6bd
AniArim <78076838+AniArim@users.noreply.github.com>
2022-11-22 06:53:24 +0300

    models created

--- auto/models.py
+++ auto/models.py
@@ -1,3 +1,27 @@
+from datetime import datetime
 from django.db import models
 
-# Create your models here.
+
+class Color(models.Model):
+	title = models.CharField(max_length=100, verbose_name='Цвет')
+
+
+class Brand(models.Model):
+	title = models.CharField(max_length=120, verbose_name='Марка автомобиля')
+
+
+class Model(models.Model):
+	title = models.CharField(max_length=120, verbose_name='Модель автомобиля')
+	brand = models.ForeignKey('Brand', on_delete=models.PROTECT, verbose_name='Марка')
+
+
+class Order(models.Model):
+	number = models.PositiveIntegerField(max_length=10, verbose_name='Номер заказа')
+	color = models.ForeignKey('Color', on_delete=models.PROTECT, verbose_name='Цвет')
+	model = models.ForeignKey('Model', on_delete=models.PROTECT, verbose_name='Модель')
+	counter = models.PositiveIntegerField(max_length=100, verbose_name='Количество')
+	date = models.DateField(default=datetime.now(), verbose_name='Дата заказа')
+
+
+
+
